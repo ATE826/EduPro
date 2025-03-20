f0665b5deb0e91efa2406c2ce5f31195908a7595
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-03-20 22:21:18 +0300

    backend files

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,1 @@
 package main
-
-import "github.com/gin-gonic/gin"
-
-func main() {
-	app := gin.Default()
-
-	app.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello, World!")
-	})
-
-	app.Run(":8080")
-}
