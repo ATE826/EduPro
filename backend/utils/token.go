8ab3c6d3c81cee879e6e54a9272fc3b23f35e48c
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-03-21 14:50:36 +0300

    env file

--- backend/utils/token.go
+++ backend/utils/token.go
@@ -1,1 +1,11 @@
 package utils
+
+import (
+	"EduPro/models"
+	"os"
+	"strconv"
+)
+
+func GenerateToken(user models.User) (string, error) { // Генерация токена
+	tokenLifespan, err := strconv.Atoi(os.Getenv("TOKEN_HOUR_LIFESPAN")) // Получение времени жизни токена из файла .env
+}
