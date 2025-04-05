a2f441ca9305f27bb33df7690de1be58547a0e6e
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-04-05 21:44:17 +0300

    navbar

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,18 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import eduProImage from './Img/EduPro.png';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { Container, Navbar, Nav, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="Header">
-        
+        <Navbar style={{ backgroundColor: 'var(--red)'}} expand="lg">
+          <Container fluid>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img className='Logo' src={eduProImage} alt="EduPro Logo" />
+              <p style={{color:'var(--orange'}}><span className='Logo-text'>EduPro</span>&copy;</p>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <button className='Btn-auth'>Войти</button>
+            </div>
+
+          </Container>
+        </Navbar>
       </header>
 
       <main>
-        sdsfsfsdfsdf
+        
       </main>
 
-      <footer className="Footer">
+      <footer>
         
       </footer>
     </div>
