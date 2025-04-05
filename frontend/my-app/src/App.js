b44be09447cad41c9027c7d0e4380eaeca943208
ToniTsey <113501444+ATE826@users.noreply.github.com>
2025-04-05 22:46:23 +0300

    header / footer

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import eduProImage from './Img/EduPro.png';
+import tgImage from './Img/tg.svg';
+import mailImage from './Img/mail.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Container, Navbar, Nav, Button } from 'react-bootstrap';
@@ -13,7 +15,7 @@ function App() {
           <Container fluid>
             <div style={{ display: 'flex', alignItems: 'center' }}>
               <img className='Logo' src={eduProImage} alt="EduPro Logo" />
-              <p style={{color:'var(--orange'}}><span className='Logo-text'>EduPro</span>&copy;</p>
+              <p style={{color:'var(--orange', marginTop: '20px'}}><span className='Logo-text'>EduPro</span>&copy;</p>
             </div>
             <div style={{ display: 'flex', alignItems: 'center' }}>
               <button className='Btn-auth'>Войти</button>
@@ -24,11 +26,23 @@ function App() {
       </header>
 
       <main>
-        
+      <div style={{height: '500px'}}>adasasdsadsadasdasdadas</div>
       </main>
 
       <footer>
-        
+      <Navbar style={{ backgroundColor: 'var(--red)'}} expand="lg">
+          <Container fluid>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <img style={{marginRight: '50px'}} className='hrefs' src={mailImage} alt="EduPro Logo" />
+              <p className='text-orange' style={{marginTop: '20px'}}>EduPro@gmail.com</p>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p className='text-orange' style={{marginTop: '20px'}}>@EduPro</p>
+              <img style={{marginLeft: '50px'}} className='hrefs' src={tgImage} alt="EduPro Logo" />
+            </div>
+
+          </Container>
+        </Navbar>
       </footer>
     </div>
   );
